67e13e332fb6644af2a83ae6f731d414fc2501ef
OleksandrVaniuta <vaniutasa@gmail.com>
2023-01-15 18:17:07 +0200

    сделали 2 задания

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let modalDict = {};
+let currentModalInstance;
+
+const galleryItemArr = makeGalleryItem(galleryItems);
+const galleryParrentEL = document.querySelector('.gallery');
+
+galleryParrentEL.insertAdjacentHTML('beforeend', galleryItemArr);
+const galleryLink = document.querySelectorAll('.gallery__link');
+galleryLink.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+});
+
+galleryParrentEL.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  console.log(event.target.parentElement);
+
+  const imgUrl = event.target.parentElement.href;
+
+  if (!modalDict[imgUrl]) {
+    modalDict[imgUrl] = basicLightbox.create(
+      `
+		<img src="${imgUrl}">
+	`
+    );
+  }
+  currentModalInstance = modalDict[imgUrl];
+  modalDict[imgUrl].show();
+});
+
+document.onkeydown = function (evt) {
+  evt = evt || window.event;
+  if (evt.key == 'Escape') {
+    currentModalInstance.close();
+  }
+};
+
+function makeGalleryItem(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="large-image.jpg"
+                        alt="${description}"
+                    />
+                 </a>
+            </div>`;
+    })
+    .join('');
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+let modalDict = {};
+let currentModalInstance;
+
+const galleryItemArr = makeGalleryItem(galleryItems);
+const galleryParrentEL = document.querySelector('.gallery');
+
+galleryParrentEL.insertAdjacentHTML('beforeend', galleryItemArr);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  additionalHtml: '<div class="alt-txt"></div>',
+});
+
+const galleryLink = document.querySelectorAll('.gallery__item');
+galleryLink.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+});
+
+gallery.on('shown.simplelightbox', function (eve) {
+  setTimeout(function () {
+    document.querySelector('.alt-txt').innerHTML =
+      eve.target.querySelector('img').alt;
+  }, 200);
+});
+
+function makeGalleryItem(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
