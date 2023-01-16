ceb889d73420c3680b1350e96bae4a468d7d406e
OleksandrVaniuta <vaniutasa@gmail.com>
2023-01-16 23:24:55 +0200

    Виправили помилки

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,35 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryItemArr = makeGalleryItem(galleryItems);
-const galleryParrentEL = document.querySelector('.gallery');
+const galleryParrentEL = document.querySelector(".gallery");
 
-galleryParrentEL.insertAdjacentHTML('beforeend', galleryItemArr);
+galleryParrentEL.insertAdjacentHTML("beforeend", galleryItemArr);
 
-let gallery = new SimpleLightbox('.gallery a', {
-  additionalHtml: '<div class="alt-txt"></div>',
+new SimpleLightbox(".gallery a", {
+	captions: true,
+	captionDelay: 250,
+	captionSelector: "img",
+	captionPosition: "bottom",
+	captionsData: "alt",
 });
 
-const galleryLink = document.querySelectorAll('.gallery__item');
-galleryLink.forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-  });
-});
-
-gallery.on('shown.simplelightbox', function (event) {
-  setTimeout(function () {
-    document.querySelector('.alt-txt').innerHTML =
-      event.target.querySelector('img').alt;
-  }, 200);
+const galleryLink = document.querySelectorAll(".gallery__item");
+galleryLink.forEach(link => {
+	link.addEventListener("click", event => {
+		event.preventDefault();
+	});
 });
 
 function makeGalleryItem(item) {
-  return item
-    .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
+	return item
+		.map(({ preview, original, description }) => {
+			return `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>`;
-    })
-    .join('');
+		})
+		.join("");
 }
